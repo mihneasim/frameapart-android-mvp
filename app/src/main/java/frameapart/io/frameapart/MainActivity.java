23b3be27f6357dff6d13a5a381af50017b542e08
Mihnea Simian <contact@mesimian.com>
2017-03-09 11:01:27 +0200

    initialized android studio project

---
+++ app/src/main/java/frameapart/io/frameapart/MainActivity.java
@@ -0,0 +1,13 @@
+package frameapart.io.frameapart;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
